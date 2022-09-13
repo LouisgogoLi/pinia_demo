cbad8737756f9cd3800942b810d21c41870e038f
LouisgogoLi <95173085+LouisgogoLi@users.noreply.github.com>
2022-09-13 02:19:47 +0000

    Deploying to gh-pages from  @ e6f861dc8a975be608f38912c5c95c741abbd8d8 🚀

---
+++ assets/golden-goldSummaryTransDetails.c12ebe08.js
@@ -0,0 +1,1 @@
+import{d as m,r as n}from"./index.c472f2aa.js";const u=m("golden/goldSummaryTransDetails",()=>{const r=n(10);function a(){r.value++}function e(){r.value--}return{summary:r,addSummary:a,minusSummary:e}});export{u};
